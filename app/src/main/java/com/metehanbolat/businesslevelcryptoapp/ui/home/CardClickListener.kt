f89bdd84171574b174fad757d93ef9bb51897a62
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-24 17:56:11 +0300

    Data Binding - CardClickListener (Interface)

---
+++ app/src/main/java/com/metehanbolat/businesslevelcryptoapp/ui/home/CardClickListener.kt
@@ -0,0 +1,7 @@
+package com.metehanbolat.businesslevelcryptoapp.ui.home
+
+import com.metehanbolat.businesslevelcryptoapp.model.success.Data
+
+interface CardClickListener {
+    fun onCardClick(coin: Data)
+}
